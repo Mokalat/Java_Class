219cbec80700e694bf78ab86ac292dd017d0e1a5
Moka <yisida70@gmail.com>
2024-04-16 10:44:39 +0900

    test:

--- Class_03/chap03-mathod-and-api/src/main/java/com/ohgiraffers/section01/Application01.java
+++ Class_03/chap03-mathod-and-api/src/main/java/com/ohgiraffers/section01/Application01.java
@@ -7,9 +7,17 @@ public class Application01 {
         메소드는 어떤 특정 작업을 수행하기 위한 명령문의 집합이라고 할 수 있다.
      */
         System.out.println("main() 시작됨....");
-        Application01 appl =
+        Application01 app1 = new Application01();
+        Application01 app2 = new Application01();
+
+        app1.meathoadA();
+        app2.meathoadB();
+
     }
-    public void main() {
+    public void meathoadA() {
         System.out.println(("meathoadA()를 호출함"));
+    }
+    public void meathoadB() {
+        System.out.println("meathoadB()를 호출해볼까?");
     }
 }
