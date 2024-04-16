746433fbe2b5048d869e7fd3019cb2bc98f23a9d
Moka <yisida70@gmail.com>
2024-04-16 10:08:04 +0900

    test

--- Class_03/chap03-mathod-and-api/src/main/java/com/ohgiraffers/section01/Application01.java
+++ Class_03/chap03-mathod-and-api/src/main/java/com/ohgiraffers/section01/Application01.java
@@ -6,9 +6,9 @@ public class Application01 {
         매소드
         메소드는 어떤 특정 작업을 수행하기 위한 명령문의 집합이라고 할 수 있다.
      */
-        System.out.println("main() 시작됨....");
+        System.out.println("main() 시작됨.... tttt");
     }
     public void main() {
-        
+
     }
 }
