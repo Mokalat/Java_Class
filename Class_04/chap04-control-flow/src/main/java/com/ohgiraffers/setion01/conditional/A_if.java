1e993383ac0f916fe5d56fc5bd2b106342caf58e
Moka <yisida70@gmail.com>
2024-04-17 18:11:23 +0900

    study

---
+++ Class_04/chap04-control-flow/src/main/java/com/ohgiraffers/setion01/conditional/A_if.java
@@ -0,0 +1,97 @@
+package com.ohgiraffers.setion01.conditional;
+
+import java.util.Scanner;
+
+public class A_if {
+    public void testSimpleIfStatement(){
+        /*
+            [if 표현식]
+                if(조건식){
+                    조건식이 참인 경우 실행할 명령문
+                }
+
+            조건식 : ture of false가 나오는 연산식을 의미한다.
+        */
+
+      /*  Scanner sc = new Scanner(System.in);
+        System.out.println("값을 입력해주세요 : ");
+        int num = sc.nextInt();
+
+        System.out.println("연산기호를 입력해주세요 : ");
+        char ch = sc.next().charAt(0);
+
+        System.out.println("값을 입력해주세요 :");
+        int num2 = sc.nextInt();*/
+
+        /* Scanner sc= new Scanner(System.in);
+         String input = sc.nextLine();
+         String[] target = input.split(" ");
+         int result = 0; */
+
+        /*
+            2의 배수면 n은 2의 배수입니다.
+            0이면 n은 0입니다.
+            2의 배수가 아니면 n은 2의 배수가 아닙니다.
+        */
+
+      /* if(num % 2 == 0){
+            System.out.println( num + "은 2의 배수 입니다. with if");
+        }else if (num % 2 == 1){
+            System.out.println( num +"은 2의 배수가 아닙니다. with else");
+        }else {
+            System.out.println(num + "해당사항이 안됩니다");
+        } */
+
+
+
+    /*  if (ch =='+'){
+            System.out.println(+num + num2 );
+        }else if (ch == '-'){
+            System.out.println( num - num2);
+        }else if (ch == '*') {
+            System.out.println(num * num2);
+        }else if (ch == '/'){
+            System.out.println(num / num2);
+        }else {
+            return ;
+        }*/
+
+    }
+   /* public String Call(){
+        Scanner sc= new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] target = input.split(" ");
+        int result = 0;
+
+        /*if(target[1] == "+"){
+            result = Integer.parseInt(target[0]) + Integer.parseInt(target[2]);
+        }
+        return "" +result;
+        *
+        if (target[1].equals("+")){
+            result = result + Integer.parseInt(target[0]);
+        }
+        String a ="1";
+        String b ="1";
+        return " " + result;
+    }*/
+
+    public double calculator(){
+        Scanner sc= new Scanner(System.in);
+        double firstNumber=sc.nextInt();
+        System.out.println("First number: "+firstNumber);
+        int secondNumber=sc.nextInt();
+        char operator=sc.next().charAt(0);
+        double result;
+        if (operator=='+'){
+            result = firstNumber+secondNumber;
+        }else if (operator=='-'){
+            result = firstNumber-secondNumber;
+        }else if (operator=='*'){
+            result = firstNumber*secondNumber;
+        }else{
+            result = firstNumber/secondNumber;
+        }
+        return 0.0;
+    }
+}
--- Class_04/chap04-control-flow/src/main/java/com/ohgiraffers/setion01/conditional/Application01.java
+++ Class_04/chap04-control-flow/src/main/java/com/ohgiraffers/setion01/conditional/Application01.java
@@ -1,4 +1,51 @@
 package com.ohgiraffers.setion01.conditional;
 
+import java.util.Scanner;
+
 public class Application01 {
+    public static void main(String[] args) {
+        A_if aIf = new A_if();
+        aIf.testSimpleIfStatement();
+
+    }
+    /*
+        계산기 만들기
+        두 수와 수식을 입력받아
+        연산기호에 맞는 연산을 수행하는 프로그램을 만들어주세요
+    */
+
+    /*
+        풀이 과정
+
+        1. calculator 메서드를 만든다.
+        2. 첫번재 수를 입력받는다.
+        3. 두번재 수를 입력받는다.
+        4. 연산 기호를 입력 받는다.
+        5. 연산 기로를 확인한다.
+        6. 연산을 진행한다.
+        7. 반환한다.
+    */
+
+
+
+    //한번에 연산식 입력받기
+
+//  public String Call(){
+//        Scanner sc= new Scanner(System.in);
+//        String input = sc.nextLine();
+//        String[] target = input.split(" ");
+//        int result = 0;
+//
+//        /*if(target[1] == "+"){
+//            result = Integer.parseInt(target[0]) + Integer.parseInt(target[2]);
+//        }
+//        return "" +result;
+//        */
+//        if (target[1].equals("+")){
+//            result = result + Integer.parseInt(target[0]);
+//        }
+//        String a ="1";
+//        String b ="1";
+//        return " " + result;
+//    }
 }
