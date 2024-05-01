b90f4a131a9c90999e03efe97621c6f5fba9d6b4
Moka <yisida70@gmail.com>
2024-05-01 18:10:36 +0900

    study

---
+++ Class_13/chap13-excpetion/src/main/java/com/ohgiraffers/sction02/userexception/exception/MoneyNegativeException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.sction02.userexception.exception;
+
+public class MoneyNegativeException extends NegativeException {
+    public MoneyNegativeException(String message) {
+        super(message);
+    }
+}
