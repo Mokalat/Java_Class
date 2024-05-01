1831595df8c867c941aa3a8b16a91b074d335df5
Moka <yisida70@gmail.com>
2024-05-01 16:22:53 +0900

    study

--- Class_13/chap13-excpetion/src/main/java/com/ohgiraffers/section01/exception/Application01.java
+++ Class_13/chap13-excpetion/src/main/java/com/ohgiraffers/section01/exception/Application01.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.section01.exception;
 public class Application01 {
     public static void main(String[] args) {
         /*
-            
+            test
         */
     }
 }
